c98a671c0acd6fbe69d48b940a88bad1c520f769
BHANU <BHANU.17y@gmail.com>
2015-03-13 20:56:34 -0300

    initial checking of the sales tool

---
+++ salesTool/src/com/demo/salestool/Main.java
@@ -0,0 +1,23 @@
+package com.demo.salestool;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		SalesData data=new SalesData();
+		
+		datadisplay();
+		data.display();
+		
+	}
+		
+		public static void datadisplay()
+		{
+			System.out.println("Hello Happy Sales People");
+			System.out.println("This app shows sales data");
+			
+		}
+	
+
+}
---
+++ salesTool/src/com/demo/salestool/SalesData.java
@@ -0,0 +1,12 @@
+package com.demo.salestool;
+
+public class SalesData {
+	
+	int data[]={01,03,45};
+	
+	public void display()
+	{
+		System.out.println("TODO: Implement data display");
+	}
+
+}
